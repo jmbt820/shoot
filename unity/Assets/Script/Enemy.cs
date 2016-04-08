a2ada1860ec224035d95c93e283181e91ac1efbd
intern <intern@intern-5.local>
2016-04-08 10:52:06 +0900

    Add randam instantiate

--- unity/Assets/Script/Enemy.cs
+++ unity/Assets/Script/Enemy.cs
@@ -9,14 +9,17 @@ public class Enemy : MonoBehaviour
 	private void Start()
 	{
 		player = GameObject.Find("Player");
+		Destroy (gameObject, 11f);
 	}
 
 	private void Update()
 	{
-		iTween.MoveTo(gameObject, player.transform.position, 10f); 
+		if (player) {
+			iTween.MoveTo(gameObject, player.transform.position, 10f); 
+		}
 	}
 
-	void OnCollisionEnter(Collision other) {
+	private void OnCollisionEnter(Collision other) {
 		if(other.gameObject.layer == 8){ //layer = 8:player
 			Destroy (other.gameObject);	
 		}
